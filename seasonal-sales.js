0f0da6261f5fec998663722828f70de5c96a77c2
Sorrel Brigman <sorrel.k@gmail.com>
2016-12-05 13:08:51 -0600

    added items to page

--- seasonal-sales.js
+++ seasonal-sales.js
@@ -7,12 +7,66 @@
     // as soon as you select the season
     // all prices should update to current discount by %
 
+
+var divForItems = document.getElementById("listOfItems");
+var itemsToPage = "";
+
+
+//function to fill in the items on the page
+// var fillItems = function() {
+//   for (var i = 0; i < products.length; i++) {
+//     var itemsToPage;
+//     itemsToPage += `<h4>${products[i].name}</h4>`;
+//     console.log(itemsToPage);
+//   }
+//   // divForItems.innerHTML = itemsToPage;
+// };
+
+//function to parse JOSN data for items
+var getProducts = function (e) {
+  var products = JSON.parse(e.target.responseText);
+  for (var i = 0; i < products.products.length; i++) {
+    if (products.products[i].category_id === 1) {
+      products.products[i].category = "Apparel";
+    } else if (products.products[i].category_id === 2) {
+      products.products[i].category = "Furniture";
+    } else if (products.products[i].category_id === 3) {
+      products.products[i].category = "Household";
+    }
+    itemsToPage += `<h3>${products.products[i].name}</h3><p>In the ${products.products[i].category} Department</p><p class="${products.products[i].category_id}">Price is: ${products.products[i].price}</p>`;
+  }
+   divForItems.innerHTML = itemsToPage;
+}
+
+
+
+
 //aquire list of products
+var storeProducts = new XMLHttpRequest();
+storeProducts.addEventListener("load", getProducts);
+storeProducts.open("GET", "products.json");
+storeProducts.send();
+
+console.log(storeProducts);
+
 //assign products to var
 //assign to div with product name, department and price
 
 
+//aquire seasons and discounts
+// var storeCat = new XMLHttpRequest();
+// storeCat.addEventListener("load", ???);
+// storeCat.open("GET", "categories.json");
+// storeCat.send();
+
+// console.log(storeCat);
+
+
 //on selection of season
-//all prices should auto update to reflect discoutn
+//all prices should auto update to reflect discount
+
+
 // write function to update prices
+
+
 //call function
