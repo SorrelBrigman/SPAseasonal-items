7e75c3a6b54c2c67051593d1d6e51a87c12fcbb1
Sorrel Brigman <sorrel.k@gmail.com>
2016-12-05 11:18:43 -0600

    broke down steps and wrote basic html template

--- seasonal-sales.js
+++ seasonal-sales.js
@@ -6,3 +6,13 @@
     //all possible values of the season_discount key in the categories file
     // as soon as you select the season
     // all prices should update to current discount by %
+
+//aquire list of products
+//assign products to var
+//assign to div with product name, department and price
+
+
+//on selection of season
+//all prices should auto update to reflect discoutn
+// write function to update prices
+//call function
