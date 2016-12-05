7c910dee1d2d98d677b1c90473fb98363fd8658d
Sorrel Brigman <sorrel.k@gmail.com>
2016-12-05 11:09:13 -0600

    wrote out direcrtions in js page, linked js page to html

---
+++ seasonal-sales.js
@@ -0,0 +1,8 @@
+//Build Webpage lists
+  //all products
+  //department product is in
+  //the price
+//put a select element at the top of the page that contains
+    //all possible values of the season_discount key in the categories file
+    // as soon as you select the season
+    // all prices should update to current discount by %
