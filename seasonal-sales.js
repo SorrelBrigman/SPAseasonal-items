db099d3555b614ce705d66911db3dadf8658f8bf
Sorrel Brigman <sorrel.k@gmail.com>
2016-12-06 08:56:51 -0600

    changed loading of categories so only needs to load once

--- seasonal-sales.js
+++ seasonal-sales.js
@@ -12,7 +12,7 @@
 var divForItems = document.getElementById("listOfItems");
 var itemsToPage = "";
 var products;
-
+var salesData;
 
 
 
@@ -28,9 +28,6 @@ var loadProducts = function () {
       products.products[i].category = "Household";
     }
 
-
-
-
 //assign to div with product name, department and price and html elements in place
     itemsToPage += `<h3>${products.products[i].name}</h3><p>In the ${products.products[i].category} Department</p><p class="${products.products[i].category_id}">Price is: <span class="price">${products.products[i].price}</span></p>`;
 }  //post to page
@@ -45,6 +42,10 @@ var getProducts = function (e) {
   loadProducts();
 }
 
+//function to parse JSON data for seasonal sales
+var getSales = function (e) {
+  salesData = JSON.parse(e.target.responseText);
+}
 
 //aquire list of products by way of products.json page loading (trigger by file load completion)
 var storeProducts = new XMLHttpRequest();
@@ -52,21 +53,25 @@ storeProducts.addEventListener("load", getProducts);
 storeProducts.open("GET", "products.json");
 storeProducts.send();
 
-
-
+// aquire seasonal discount by way of categories.json page loading (trigger by file load completion)
+var storeCat = new XMLHttpRequest();
+storeCat.addEventListener("load", getSales);
+storeCat.open("GET", "categories.json");
+storeCat.send();
+console.log(storeCat);
 //all prices should auto update to reflect discount (trigger by change of option below)
 // write function to update prices
 var changePrice = function (e) {
-  var data = JSON.parse(e.target.responseText);
+
   for (var i = 0; i < 3; i++) {
-    if (document.getElementById("seasons").value === data.categories[i].season_discount) {
-      var currentClass = data.categories[i].id;
+    if (document.getElementById("seasons").value === salesData.categories[i].season_discount) {
+      var currentClass = salesData.categories[i].id;
       var whichClass =  document.getElementsByClassName(currentClass);
       for (var j = 0; j < whichClass.length; j++) {
         // var seasonProduct = whichClass[j].toString();
         var price = parseFloat(whichClass[j].getElementsByTagName("span")[0].innerHTML);
         console.log("price" + price)
-        var newPrice = price * (1 - (parseFloat(data.categories[i].discount)));
+        var newPrice = price * (1 - (parseFloat(salesData.categories[i].discount)));
         console.log("price after discount" + Math.round(newPrice) * 100)/100;
         newPrice = Math.round(newPrice * 100) / 100;
         whichClass[j].getElementsByTagName("span")[0].innerHTML = newPrice;
@@ -81,20 +86,18 @@ var changePrice = function (e) {
 
 //clear previous changed content by reloading page before changes
 document.querySelector("select").addEventListener("change", function (e) {
-  var storeProducts = new XMLHttpRequest();
-storeProducts.addEventListener("load", getProducts);
-storeProducts.open("GET", "products.json");
-storeProducts.send();
+  loadProducts();
+  changePrice();
 });
 
 
 
-//call function
-document.querySelector("select").addEventListener("change", function (e) {
-  //aquire seasons and discounts
-  var storeCat = new XMLHttpRequest();
-  storeCat.addEventListener("load", changePrice);
-  storeCat.open("GET", "categories.json");
-  storeCat.send();
-  console.log(storeCat);
-});
+// //call function
+// document.querySelector("select").addEventListener("change", function (e) {
+//   //aquire seasons and discounts
+//   var storeCat = new XMLHttpRequest();
+//   storeCat.addEventListener("load", changePrice);
+//   storeCat.open("GET", "categories.json");
+//   storeCat.send();
+//   console.log(storeCat);
+// });
